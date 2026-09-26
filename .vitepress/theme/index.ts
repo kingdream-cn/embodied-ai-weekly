d7ed5caceb1e60dabe44caeb2ec18cdb3708154b
123 <15639422795@163.com>
2026-09-26 14:59:41 +0800

    chore: 移除已废弃的 MediaEmbed 组件及其注册

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,13 +2,11 @@ import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import './style.css'
 import IssuesGrid from './components/IssuesGrid.vue'
-import MediaEmbed from './components/MediaEmbed.vue'
 
 const theme: Theme = {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('IssuesGrid', IssuesGrid)
-    app.component('MediaEmbed', MediaEmbed)
   },
 }
 
